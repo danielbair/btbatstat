fc53273d17ba9277e9694dfdafc522d774d02174
Joris Vandalon <wiebel@gmail.com>
2011-07-28 10:46:39 +0200

    seperate icons for mighty and magic mouse. Changed update interval to 10 seconds

--- BtBatStat.py
+++ BtBatStat.py
@@ -22,13 +22,15 @@ start_time = NSDate.date()
 class Timer(NSObject):
   statusbar = None
   KeyBat = None
-  MouseBat = None
+  MagicMouseBat = None
+  MightyMouseBat = None
   TPBat = None
   noDevice = None
 
   # Load images
   kbImage = NSImage.alloc().initByReferencingFile_('kb.png')
-  mouseImage = NSImage.alloc().initByReferencingFile_('mouse.png')
+  magicImage = NSImage.alloc().initByReferencingFile_('magic_mouse.png')
+  mightyImage = NSImage.alloc().initByReferencingFile_('mighty_mouse.png')
   tpImage = NSImage.alloc().initByReferencingFile_('TrackpadIcon.png')
   noDeviceImage = NSImage.alloc().initByReferencingFile_('no_device.png')
 
@@ -49,7 +51,7 @@ class Timer(NSObject):
     self.menu.addItem_(menuQuit)
 
     # Get the timer going
-    self.timer = NSTimer.alloc().initWithFireDate_interval_target_selector_userInfo_repeats_(start_time, 5.0, self, 'tick:', None, True)
+    self.timer = NSTimer.alloc().initWithFireDate_interval_target_selector_userInfo_repeats_(start_time, 10.0, self, 'tick:', None, True)
     NSRunLoop.currentRunLoop().addTimer_forMode_(self.timer, NSDefaultRunLoopMode)
     self.timer.fire()
 
@@ -66,20 +68,27 @@ class Timer(NSObject):
     else:
 	KeyBatStat = None
 
-    MouseBatStatCmd = subprocess.Popen(["ioreg", "-rc", "AppleBluetoothHIDMouse"], stdout=subprocess.PIPE).communicate()[0]
-    if MouseBatStatCmd == "":
-        MouseBatStatCmd = subprocess.Popen(["ioreg", "-rc", "BNBMouseDevice"], stdout=subprocess.PIPE).communicate()[0]
-    MouseBatStatCmdOut = re.search('BatteryPercent" = (\d{1,2})', MouseBatStatCmd)
-    if MouseBatStatCmdOut:
-	MouseBatStat = MouseBatStatCmdOut.group(1)
+    MightyMouseBatStatCmd = subprocess.Popen(["ioreg", "-rc", "AppleBluetoothHIDMouse"], stdout=subprocess.PIPE).communicate()[0]
+    if MightyMouseBatStatCmd:
+    	MightyMouseBatStatCmdOut = re.search('BatteryPercent" = (\d{1,2})', MightyMouseBatStatCmd)
+	MightyMouseBatStat = MightyMouseBatStatCmdOut.group(1)
         if debug:
-            print "Mouse battery: ", KeyBatStat
+            print "Mighty Mouse battery: ", KeyBatStat
     else:
-	MouseBatStat = None
+	MightyMouseBatStat = None
+
+    MagicMouseBatStatCmd = subprocess.Popen(["ioreg", "-rc", "BNBMouseDevice"], stdout=subprocess.PIPE).communicate()[0]
+    if MagicMouseBatStatCmd:
+    	MagicMouseBatStatCmdOut = re.search('BatteryPercent" = (\d{1,2})', MagicMouseBatStatCmd)
+	MagicMouseBatStat = MagicMouseBatStatCmdOut.group(1)
+        if debug:
+            print "Magic Mouse battery: ", KeyBatStat
+    else:
+	MagicMouseBatStat = None
 
     TPBatStatCmd = subprocess.Popen(["ioreg", "-rc", "BNBTrackpadDevice"], stdout=subprocess.PIPE).communicate()[0]
-    TPBatStatCmdOut = re.search('BatteryPercent" = (\d{1,2})', TPBatStatCmd)
-    if TPBatStatCmdOut:
+    if TPBatStatCmd:
+    	TPBatStatCmdOut = re.search('BatteryPercent" = (\d{1,2})', TPBatStatCmd)
 	TPBatStat = TPBatStatCmdOut.group(1)
         if debug:
             print "Trackpad battery: ", KeyBatStat
@@ -99,16 +108,27 @@ class Timer(NSObject):
       self.statusbar.removeStatusItem_(self.KeyBat)
       self.KeyBat = None
 
-    if MouseBatStat:
-      if self.MouseBat is None:
-        self.MouseBat = self.statusbar.statusItemWithLength_(NSVariableStatusItemLength)
-	self.MouseBat.setImage_(self.mouseImage)
-        self.MouseBat.setHighlightMode_(1)
-        self.MouseBat.setMenu_(self.menu)
-      self.MouseBat.setTitle_(MouseBatStat +'%')
-    elif self.MouseBat is not None:
-      self.statusbar.removeStatusItem_(self.MouseBat)
-      self.MouseBat = None
+    if MightyMouseBatStat:
+      if self.MightyMouseBat is None:
+        self.MightyMouseBat = self.statusbar.statusItemWithLength_(NSVariableStatusItemLength)
+	self.MightyMouseBat.setImage_(self.mightyImage)
+        self.MightyMouseBat.setHighlightMode_(1)
+        self.MightyMouseBat.setMenu_(self.menu)
+      self.MightyMouseBat.setTitle_(MightyMouseBatStat +'%')
+    elif self.MightyMouseBat is not None:
+      self.statusbar.removeStatusItem_(self.MightyMouseBat)
+      self.MightyMouseBat = None
+
+    if MagicMouseBatStat:
+      if self.MagicMouseBat is None:
+        self.MagicMouseBat = self.statusbar.statusItemWithLength_(NSVariableStatusItemLength)
+	self.MagicMouseBat.setImage_(self.magicImage)
+        self.MagicMouseBat.setHighlightMode_(1)
+        self.MagicMouseBat.setMenu_(self.menu)
+      self.MagicMouseBat.setTitle_(MagicMouseBatStat +'%')
+    elif self.MagicMouseBat is not None:
+      self.statusbar.removeStatusItem_(self.MagicMouseBat)
+      self.MagicMouseBat = None
 
     if TPBatStat:
       if self.TPBat is None:
@@ -121,13 +141,13 @@ class Timer(NSObject):
       self.statusbar.removeStatusItem_(self.TPBat)
       self.TPBat = None
 
-    if self.MouseBat is None and self.KeyBat is None and self.TPBat is None and self.noDevice is None:
+    if self.MagicMouseBat is None and self.KeyBat is None and self.TPBat is None and self.noDevice is None:
 	self.noDevice = self.statusbar.statusItemWithLength_(NSVariableStatusItemLength)
 	self.noDevice.setImage_(self.noDeviceImage)
         self.noDevice.setHighlightMode_(1)
         self.noDevice.setMenu_(self.menu)
         self.noDevice.setToolTip_('BtBatStat: No Apple mouse or keyboard found!')
-    elif (self.MouseBat is not None or self.KeyBat is not None) and self.noDevice is not None:
+    elif (self.MagicMouseBat is not None or self.KeyBat is not None) and self.noDevice is not None:
 	self.statusbar.removeStatusItem_(self.noDevice)
 	self.noDevice = None
 
